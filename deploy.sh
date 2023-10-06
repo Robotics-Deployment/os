6ebbe34a15da042f5f30c82a41d63b34465e30ba
deniz <deniz.hofmeister@gmail.com>
2023-10-06 10:17:28 +0200

    removed

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker login registry.digitalocean.com
-
-docker tag robotics-deployment:os registry.digitalocean.com/rdcr/robotics-deployment:os
-docker push registry.digitalocean.com/rdcr/robotics-deployment:os
